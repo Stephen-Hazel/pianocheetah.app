a1712c222b8651f6b20332d2ea54ad4cd963889b
Stephen Hazel <stephen.hazel@gmail.com>
2026-02-15 19:08:01 -0800

    more

--- shaz.app/song/index.php
+++ shaz.app/song/index.php
@@ -269,11 +269,11 @@ $(function () {                        // boot da page
    $('#info tbody').on ('click','tr',function ()  { next ($(this).index ()); });
 });
 /*
-"https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1"
+"https://www.gstatic.com/cast/sdk/libs/caf_sender/v3/cast_framework.js"
 */
  </script>
  <script src=
-"https://www.gstatic.com/cast/sdk/libs/caf_sender/v3/cast_framework.js"
+"https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1"
   ></script>
 
 <? pg_body ([ [$UC['home']." home",  "..",  "...take me back hooome"] ]); ?>

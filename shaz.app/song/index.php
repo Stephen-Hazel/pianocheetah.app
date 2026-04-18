cae9c154125d567905ff887404ca126dedfb0a21
Stephen Hazel <stephen.hazel@gmail.com>
2026-04-18 14:50:08 -0700

    reverttt

--- shaz.app/song/index.php
+++ shaz.app/song/index.php
@@ -1,72 +1,72 @@
-<? # song/cast.php - play ma songs
+<? # song/index.php - cast n play ma songs
 
 require_once ("../_inc/app.php");
 
-   $shuf = arg ('shuf','Y');
-   $pick = [];
-   foreach (explode (',', arg ('pick')) as $p)  if ($p != '')  $pick[] = $p;
+$shuf = arg ('shuf','Y');
+$pick = [];
+foreach (explode (',', arg ('pick')) as $p)  if ($p != '')  $pick[] = $p;
 
 ## doin a scoot?
-   if (($fr = arg ('sc')) != '')       ## $to needs from dir chopped off
-      {$to = substr ($fr, 3);   rename ("song/$fr", "song/_z/$to");}
+if (($fr = arg ('sc')) != '')       ## $to needs from dir chopped off
+{$to = substr ($fr, 3);   rename ("song/$fr", "song/_z/$to");}
 
-   $dir = [];                          ## build dir[] from song dirs minus _z
-   foreach (LstDir ("song", 'd') as $d)  if ($d != '_z')  $dir[] = $d;
-   sort ($dir);
-   $dirp = [];                         ## dirp is names of picked dirs
-   foreach ($dir as $i => $d)  if (in_array ($i, $pick))  $dirp[] = $d;
+$dir = [];                          ## build dir[] from song dirs minus _z
+foreach (LstDir ("song", 'd') as $d)  if ($d != '_z')  $dir[] = $d;
+sort ($dir);
+$dirp = [];                         ## dirp is names of picked dirs
+foreach ($dir as $i => $d)  if (in_array ($i, $pick))  $dirp[] = $d;
 
 ## build pld[] given dirp's files  (minus did[] if shuffle)
-   $did = ($shuf == 'N') ? [] : explode ("\n", Get ("did.txt"));
-   $pld = [];
-   foreach ($dirp as $i => $d) {
-      $pld [$i] = [];
-      foreach (LstDir ("song/$d", 'f') as $fn)
-         if (! in_array ("$d/$fn", $did))  $pld [$i][] = "$d/$fn";
+$did = ($shuf == 'N') ? [] : explode ("\n", Get ("did.txt"));
+$pld = [];
+foreach ($dirp as $i => $d) {
+   $pld [$i] = [];
+   foreach (LstDir ("song/$d", 'f') as $fn)
+      if (! in_array ("$d/$fn", $did))  $pld [$i][] = "$d/$fn";
       if (($shuf == 'Y') && (count ($pld [$i]) == 0)) {
          unlink ("did.txt");           ## time ta kill did.txt
          header ("Location: ?shuf=".$shuf."&pick=".arg ('pick'));
       }
-   }
-   $pl = [];
-   if ($shuf == 'Y') {                 ## shuffle and interleave dirs
-      foreach ($dirp as $i => $d)  shuffle ($pld [$i]);
-      for ($i = 0;;  $i++) {
-         $got = 0;
-         foreach ($dirp as $j => $d)
-            if (aHas ($pld [$j], $i))  {$got = 1;   $pl[] = $pld [$j][$i];}
+}
+$pl = [];
+if ($shuf == 'Y') {                 ## shuffle and interleave dirs
+   foreach ($dirp as $i => $d)  shuffle ($pld [$i]);
+   for ($i = 0;;  $i++) {
+      $got = 0;
+      foreach ($dirp as $j => $d)
+         if (aHas ($pld [$j], $i))  {$got = 1;   $pl[] = $pld [$j][$i];}
          if (! $got)  break;
-      }
+   }
+}
+else {
+   foreach ($dirp as $i => $d)  foreach ($pld [$i] as $f)  $pl[] = $f;
+   usort ($pl, function ($a, $b) {  ## skip dir name in sort
+      $a1 = substr ($a, strpos ($a, '/')+1);
+      $b1 = substr ($b, strpos ($b, '/')+1);
+      if ($a1 == $b1)  return 0;
+      return ($a1 < $b1) ? -1 : 1;
+   });
+}
+$nm = [];
+foreach ($pl as $i => $s) {         ## pretty up the name
+   $d = substr ($s, 0, strpos ($s, '/'));
+   $s = substr ($s, strlen ($d)+1);      ## toss leading dir/
+   $s = substr ($s, 0, -4);              ## toss .mp3
+   $s = str_replace ('_', ' ', $s);      ## _ => space
+   $f = strpos  ($s, '-');
+   $l = strrpos ($s, '-');
+   if ($f !== false) {                   ## l musta been set too
+      $g = substr ($s, 0, $f);           ## but they shouldn't be the same!
+      $t = substr ($s, $l+1);
+      $x = ($f == $l) ? '' : substr ($s, $f+1, $l-$f-1);
+      $s = "$g\n$x\n$t\n$d";
    }
    else {
-      foreach ($dirp as $i => $d)  foreach ($pld [$i] as $f)  $pl[] = $f;
-      usort ($pl, function ($a, $b) {  ## skip dir name in sort
-         $a1 = substr ($a, strpos ($a, '/')+1);
-         $b1 = substr ($b, strpos ($b, '/')+1);
-         if ($a1 == $b1)  return 0;
-         return ($a1 < $b1) ? -1 : 1;
-      });
+      #dbg($s);
+      $s = "?? $s $d";
    }
-   $nm = [];
-   foreach ($pl as $i => $s) {         ## pretty up the name
-      $d = substr ($s, 0, strpos ($s, '/'));
-      $s = substr ($s, strlen ($d)+1);      ## toss leading dir/
-      $s = substr ($s, 0, -4);              ## toss .mp3
-      $s = str_replace ('_', ' ', $s);      ## _ => space
-      $f = strpos  ($s, '-');
-      $l = strrpos ($s, '-');
-      if ($f !== false) {                   ## l musta been set too
-         $g = substr ($s, 0, $f);           ## but they shouldn't be the same!
-         $t = substr ($s, $l+1);
-         $x = ($f == $l) ? '' : substr ($s, $f+1, $l-$f-1);
-         $s = "$g\n$x\n$t\n$d";
-      }
-      else {
-#dbg($s);
-         $s = "?? $s $d";
-      }
-      $nm[] = $s;
-   }
+   $nm[] = $s;
+}
 
 pg_head ("song", "jqui app", "jqui app");
 ?>
@@ -105,7 +105,6 @@ let PL = <?= json_encode ($pl); ?>;    // play list array
 let Nm = <?= json_encode ($nm); ?>;    // prettier names w group,title,etc,dir
 let Tk = 0;                            // pos of track we're on
 let CastOK = false;
-let lastContentId = '';
 
 function shuf ()  {return $('#shuf').is (':checked') ? 'Y':'N';}
 
@@ -128,47 +127,53 @@ function kick (newtk)
 // song got clicked on - make remake queue from there
 {  sess = cast.framework.CastContext.getInstance ().getCurrentSession ();
    if ((! sess) || (! CastOK))
-      {alert ("ya ain't castin yet i think ?");   return;}
+   {alert ("ya ain't castin yet i think ?");   return;}
 
-  let player = new cast.framework.RemotePlayer ();
-  let plCtl  = new cast.framework.RemotePlayerController (player);
+   let player = new cast.framework.RemotePlayer ();
+   let plCtl  = new cast.framework.RemotePlayerController (player);
    plCtl.stop ();                      // SHUSH !
 
-dbg("kick newtk="+newtk);
+   dbg("kick newtk="+newtk);
    $('#info tbody tr').eq (Tk).css ("background-color", "");    // unhilite
    Tk = newtk;
-   lastContentId = '';
 
    if ((pick ().length > 0) && (PL.length == 0))  redo (); // outa songs!
    if (Tk >= PL.length)  return;
 
-  let mo = [];
+   let mo = [];
    for (o = 0;  o < 50;  o++) {
-     let i = Tk+o;
+      let i = Tk+o;
       if (i >= PL.length)  break;
 
-     let ar = Nm [i].split ("\n");
+      let ar = Nm [i].split ("\n");
       if (o == 0) {
          document.title = ar [2] + ' - ' + ar [0];
          $('#info tbody tr').eq (Tk).css ("background-color", "#FFFF80;");
       }
-     let mi = {
+      let mi = {
          contentId:   'https://shaz.app/song/song/' + PL [i],
          contentType: 'audio/mpeg',
          metadata:    { metadataType: 0, artist: ar [0], title: ar [2] }
       };
       mo [o] = { media: mi, autoplay: true };
+
+
+
+
    }
-dbg("queuein' "+mo.length);
+   dbg("queuein' "+mo.length);
    sess.loadMedia ({ queueData: { items: mo } });
-dbg('playin!');
+
+
+
+   dbg('playin!');
 }
 
 
 function lyr ()                        // hit google lookin fo lyrics
 {  if (Tk >= PL.length)  return;
 
-  let a = Nm [Tk].split ("\n");   tt = a [2];   gr = a [0];
+   let a = Nm [Tk].split ("\n");   tt = a [2];   gr = a [0];
    window.open ('https://google.com/search?q=lyrics "'+tt+'" "'+gr+'"',
                 "lyrics");
 }
@@ -178,61 +183,43 @@ function scoot ()  { redo ('&sc=' + PL [Tk]); }
 
 
 window ['__onGCastApiAvailable'] = function (avail) {
-dbg("__onGCastApiAvailable avail="+avail);
    if (! avail)  return;
 
    cast.framework.CastContext.getInstance ().setOptions ({
       receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
       autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
    });
-  let player = new cast.framework.RemotePlayer ();
-  let plCtl  = new cast.framework.RemotePlayerController (player);
-  let didSet = new Set ();
-
-
+   let player = new cast.framework.RemotePlayer ();
+   let plCtl  = new cast.framework.RemotePlayerController (player);
    plCtl.addEventListener (
       cast.framework.RemotePlayerEventType.PLAYER_STATE_CHANGED,
       (event) => {
-         if (event.value != 'IDLE')  return;
-        let ci = ((player.mediaInfo ?? '') == '') ? ''
-                  : player.mediaInfo.contentId;
-         if (ci == '')  return;
-        let fn = ci.substr (27);
-dbg("idle done='"+fn+"'");
-         if (! didSet.has (fn)) {
-            didSet.add (fn);
+         if (event.value == 'IDLE') {
+            dbg("player");dbg(player);
+            if ((player.mediaInfo ?? '') == '')  return;
+
+            let fn = player.mediaInfo.contentId.substr (27);
+            dbg("done='"+fn+"'");
             $.get ("did.php", { did: fn });
             if (player.currentTime > 5) {
                $.get ("skip.php", { it: fn });
-dbg("   WAS SKIPPED!");
+               dbg("   WAS SKIPPED!");
             }
+
+            // unhilite old
+            $('#info tbody tr').eq (Tk).css ("background-color", "");
+
+            Tk += 1;
+            if (Tk >= PL.length)  return;
+
+            // title and hilite
+            let a = Nm [Tk].split ("\n");   tt = a [2];   gr = a [0];
+            document.title = tt + ' - ' + gr;
+
+            $('#info tbody tr').eq (Tk).css ("background-color", "#FFFF80;");
          }
       }
    );
-
-   setInterval (() => {
-     let ci = ((player.mediaInfo ?? '') == '') ? ''
-               : player.mediaInfo.contentId;
-      if (ci == '' || ci == lastContentId)  return;
-dbg("poll nextsong ci="+ci.substr(27));
-      if (lastContentId != '') {
-        let fn = lastContentId.substr (27);
-         if (! didSet.has (fn)) {
-            didSet.add (fn);
-            $.get ("did.php", { did: fn });
-         }
-         $('#info tbody tr').eq (Tk).css ("background-color", "");
-        let newIdx = PL.indexOf (ci.substr (27));
-         Tk = (newIdx >= 0) ? newIdx : Tk + 1;
-         if (Tk < PL.length) {
-           let a = Nm [Tk].split ("\n");
-            document.title = a [2] + ' - ' + a [0];
-            $('#info tbody tr').eq (Tk)
-               .css ("background-color", "#FFFF80;");
-         }
-      }
-      lastContentId = ci;
-   }, 2000);
    CastOK = true;
 };
 
@@ -249,24 +236,24 @@ $(function () {                        // boot da page
 });
 </script>
 <script src=
-   "https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1">
+"https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1">
 </script>
 
 <? pg_body ([ [$UC['home']." home",  "..",  "...take me back hooome"] ]); ?>
 <span style="padding-left: 5em"></span>
 <? check ('shuf', 'shuf', $shuf);
-   foreach ($dir as $i => $s)
-      check ("chk$i", $s, in_array ($i, $pick) ? 'Y':''); ?>
-<span id='num'><?= count($nm) ?></span><br class='mobl'>
-<a id='lyr'>lyric</a>
-<google-cast-launcher></google-cast-launcher>
+foreach ($dir as $i => $s)
+   check ("chk$i", $s, in_array ($i, $pick) ? 'Y':''); ?>
+   <span id='num'><?= count($nm) ?></span><br class='mobl'>
+   <a id='lyr'>lyric</a>
+   <google-cast-launcher></google-cast-launcher>
 
-<? $n2 = [];
-   foreach ($nm as $n) {
-      $a = explode ("\n", $n);
-      if (($shuf == 'N') && (count ($pick) >= 4))
-           $n2[] = "<b>".$a [0]."</b>"." ".$a [1]." <b>".$a [2]."</b> ".$a [3];
-      else $n2[] = "<b>".$a [2]."</b>"." ".$a [0]." <b>".$a [3]."</b> ".$a [1];
-   }
-   table1 ('info', '', $n2); ?>
+   <? $n2 = [];
+foreach ($nm as $n) {
+   $a = explode ("\n", $n);
+   if (($shuf == 'N') && (count ($pick) >= 4))
+      $n2[] = "<b>".$a [0]."</b>"." ".$a [1]." <b>".$a [2]."</b> ".$a [3];
+   else $n2[] = "<b>".$a [2]."</b>"." ".$a [0]." <b>".$a [3]."</b> ".$a [1];
+}
+table1 ('info', '', $n2); ?>
 <? pg_foot ();
